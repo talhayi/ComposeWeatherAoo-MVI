e553d2e0879ac2c41561cbeba225c8d787a8e43a
talhayi <talhayldrm23@gmail.com>
2024-05-27 13:16:12 +0300

    Created Dagger Hilt

---
+++ app/src/main/java/com/example/composeweatherapp/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.example.composeweatherapp.di
+
+import com.example.composeweatherapp.data.repository.WeatherRepositoryImpl
+import com.example.composeweatherapp.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
